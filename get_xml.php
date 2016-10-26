bca80181b8110c85be3e283c953e0342665be1d4
Tokiwa <tkw.4402@gmail.com>
2016-10-26 13:26:04 +0900

    Update hw2.js - 3

--- get_xml.php
+++ get_xml.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_GET["url"]) && preg_match("/^http?:/",$_GET["url"])){
+if(isset($_GET["url"]) && preg_match("/^https?:/",$_GET["url"])){
   header('Content-type: application/xml');
   $req_url = $_GET['url'];
   foreach ( $_GET as $key => $value){
